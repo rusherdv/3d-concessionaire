71bba1b2193dad0db038635a9a2c87d79f25fa28
Rusher <fdecabanas2004@gmail.com>
2024-04-25 18:41:53 -0300

    Update

--- src/components/carvisualizer.jsx
+++ src/components/carvisualizer.jsx
@@ -1,119 +1,124 @@
-import { useEffect, useState, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { useGLTF, Stage, OrbitControls } from '@react-three/drei';
+import { useEffect, useState, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, OrbitControls } from "@react-three/drei";
 
 function Model(props) {
-  const [ currentCar, setCurrentCar ] = useState(props.car)
+  const [currentCar, setCurrentCar] = useState(props.car);
   const { scene, materials } = useGLTF(currentCar);
 
-  if(currentCar === '/supra.glb'){
-
+  if (currentCar === "/supra.glb") {
     useEffect(() => {
-      materials['Toyota_Supra_2020_with_Interior_noTTo3Ds_carpaint.001'].color.set(props.color);
-      materials['Toyota_Supra_2020_with_Interior_noTTo3Ds_leatherRed.001'].color.set(props.color);
+      materials[
+        "Toyota_Supra_2020_with_Interior_noTTo3Ds_carpaint.001"
+      ].color.set(props.color);
+      materials[
+        "Toyota_Supra_2020_with_Interior_noTTo3Ds_leatherRed.001"
+      ].color.set(props.color);
     }, [materials, props.color]);
-  
-    useEffect(() => {
-      materials['Toyota_Supra_2020_with_Interior_noTTo3Ds_rimB.001'].color.set(props.wheelColor);
-    }, [materials, props.wheelColor]);
-    
-    return <primitive object={scene} {...props} />;
-  
-  }else if (currentCar === '/svj.glb'){
 
     useEffect(() => {
-      materials['CARROSSERIE'].color.set(props.color);
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['Material.012'].color.set(props.wheelColor);
+      materials["Toyota_Supra_2020_with_Interior_noTTo3Ds_rimB.001"].color.set(
+        props.wheelColor
+      );
     }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  
-  }else if (currentCar === '/porsche911turbos.glb'){
+  } else if (currentCar === "/svj.glb") {
+    useEffect(() => {
+      materials["CARROSSERIE"].color.set(props.color);
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['paint'].color.set(props.color);
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['Material.001'].color.set(props.wheelColor);
-    },[materials, props.wheelColor])
+      materials["Material.012"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  }else if (currentCar === '/bmw.glb'){
+  } else if (currentCar === "/porsche911turbos.glb") {
+    useEffect(() => {
+      materials["paint"].color.set(props.color);
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['body'].color.set(props.color);
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['etriers'].color.set(props.wheelColor);
-    },[materials, props.wheelColor])
+      materials["Material.001"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  }else if (currentCar === '/a5.glb'){
+  } else if (currentCar === "/bmw.glb") {
+    useEffect(() => {
+      materials["body"].color.set(props.color);
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['2021_Audi_A5_Sportback_S-Line_Ibis_White'].color.set(props.color);
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['2021_Audi_A5_Sportback_S-Line_Rims1'].color.set(props.wheelColor);
-    },[materials, props.wheelColor])
+      materials["etriers"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  }else if (currentCar === '/mustang.glb'){
+  } else if (currentCar === "/a5.glb") {
+    useEffect(() => {
+      materials["2021_Audi_A5_Sportback_S-Line_Ibis_White"].color.set(
+        props.color
+      );
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['carpaint'].color.set(props.color);
-      materials['leather'].color.set(props.color);
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['brake'].color.set(props.wheelColor);
-    },[materials, props.wheelColor])
+      materials["2021_Audi_A5_Sportback_S-Line_Rims1"].color.set(
+        props.wheelColor
+      );
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  } else if (currentCar === '/koenigsegg.glb'){
+  } else if (currentCar === "/mustang.glb") {
+    useEffect(() => {
+      materials["carpaint"].color.set(props.color);
+      materials["leather"].color.set(props.color);
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['material_17'].color.set(props.color)
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['Material.015'].color.set(props.wheelColor);
-    },[materials, props.wheelColor])
+      materials["brake"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  } else if (currentCar === '/challenger.glb'){
+  } else if (currentCar === "/koenigsegg.glb") {
+    useEffect(() => {
+      materials["material_17"].color.set(props.color);
+    }, [materials, props.color]);
 
     useEffect(() => {
-      materials['Carosserie'].color.set(props.color)
-    },[materials, props.color])
-
-    useEffect(() => {
-      materials['Material.009'].color.set(props.wheelColor)
-    },[materials, props.wheelColor])
+      materials["Material.015"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
 
     return <primitive object={scene} {...props} />;
-  } 
-  
+  } else if (currentCar === "/challenger.glb") {
+    useEffect(() => {
+      materials["Carosserie"].color.set(props.color);
+    }, [materials, props.color]);
+
+    useEffect(() => {
+      materials["Material.009"].color.set(props.wheelColor);
+    }, [materials, props.wheelColor]);
+
+    return <primitive object={scene} {...props} />;
+  }
+
   return <primitive object={scene} {...props} />;
-
 }
 
 export function CarVisualizer(props) {
   return (
     <>
       <Canvas dpr={[1, 2]} camera={{ fov: 60 }} className="absolute">
-        <color attach="background" args={['#101010']} />
+        <color attach="background" args={["#101010"]} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         <OrbitControls />
-        <Stage environment={'sunset'}>
-          <Model car={props.car.object} scale={1} color={props.color} wheelColor={props.wheelColor} rotation={[0, 6, 0]} />
+        <Stage environment={"sunset"}>
+          <Model
+            car={props.car.object}
+            scale={1}
+            color={props.color}
+            wheelColor={props.wheelColor}
+            rotation={[0, 6, 0]}
+          />
         </Stage>
       </Canvas>
     </>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
-import cars from '../../public/db.json';
-import {Button} from "@nextui-org/react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import cars from "../../public/db.json";
+import { Button } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
-  const [car, setCar] = useState(cars)
+  const [car, setCar] = useState(cars);
 
   return (
     <>
-      <div className='bg-black h-[100vh] flex justify-center items-center flex-col'>
-        <p className='text-white text-4xl inter600'>3D Concessionaire</p>
-        <div className='mt-10 flex flex-wrap w-9/12 justify-center max-2xl:w-full overflow-y-auto max-2xl:h-3/6'>
-          {car.cars.map((item, index) => (
-            <div key={index} className='inter400 text-white mr-5 mt-5 w-[300px] border-2 border-gray-800 p-5'>
-              <img src={item.img} className='w-[300px] h-[150px] object-cover' alt="" />
-              <p className='inte600 text-gray-500 text-xs ml-2 mt-2'>{item.brand}</p>
-              <p className='inter600 text-xl ml-2'>{item.model}</p>
+      <div className="bg-black h-full flex justify-start items-center flex-col">
+        {car.cars.map((item, index) => (
+          <div
+            key={index}
+            className="inter400 text-white h-screen mr-5 w-screen relative"
+          >
+            <img
+              src={item.img}
+              className="w-screen h-screen object-cover opacity-30"
+              alt=""
+            />
+            <div className="absolute bottom-20 left-20">
+              <p className="inter600 text-5xl">{item.brand}</p>
+              <p className="inter600 text-6xl">{item.model}</p>
               <Link to={`/car/${item.id}`}>
-                <Button color="danger" variant='ghost' className='rounded-none mt-2 w-full inter600'>Inspect</Button>
+                <Button
+                  color="danger"
+                  variant="ghost"
+                  className="rounded-none w-full inter600 mt-4"
+                >
+                  Inspect
+                </Button>
               </Link>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </>
   );
